8d658d2162cee47bbb4e0cb45b2850db76b55ce7
techsphere-brian <techsphereke1@gmail.com>
2024-11-17 09:25:33 +0300

    Database migrations

--- tech/models.py
+++ tech/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+   fname = models.CharField(max_length=50)
+   lname = models.CharField(max_length=50)
+   email = models.EmailField(max_length=100)
+   age = models.PositiveIntegerField()
+   phone = models.CharField(max_length=50)
+   profile = models.ImageField(upload_to='profiles/')
+   
+   def __str__(self):
+       return f'{self.fname}{self.lname}'
--- tech/views.py
+++ tech/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . import models
 
 # Create your views here.
+def home(request):
+   students = models.Student.objects.all()
+   return render(request, 'tech/home.html', {'students': students})
+def student(request):
+   if request.method == 'POST':
+      fname = request.POST['fname']
+      lname = request.POST['lname']
+      email = request.POST['email']
+      age = request.POST['age']
+      phone = request.POST['phone']
+      profile = request.FILES['profile']
+      
+      # Save the data to the database
+      # (Assuming 'Student' is a model in your database)
+      student = models.Student(fname=fname, lname=lname, email=email, age=age, phone=phone, profile=profile)
+      student.save()
+      return redirect('home')
+      
+   return render(request, 'tech/student.html')
